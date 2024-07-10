29fa39f37eaf403c64698847f57ef84e66c22a32
truongnm-0478 <ngo.mau.truong@sun-asterisk.com>
2024-07-10 12:27:56 +0700

    hibernate module [One-to-Many relationship, Many-to-One relationship]

---
+++ hibernate-modules/ex3_One-to-Many_Many-to-One/src/main/java/model/Department.java
@@ -0,0 +1,62 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL)
+    private Set<Employee> employees = new HashSet<>();
+
+    public Department() {
+    }
+
+    public Department(Long id, String name, Set<Employee> employees) {
+        this.id = id;
+        this.name = name;
+        this.employees = employees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public void addEmployee(Employee employee) {
+        employees.add(employee);
+        employee.setDepartment(this);
+    }
+
+    public void removeEmployee(Employee employee) {
+        employees.remove(employee);
+        employee.setDepartment(null);
+    }
+}
