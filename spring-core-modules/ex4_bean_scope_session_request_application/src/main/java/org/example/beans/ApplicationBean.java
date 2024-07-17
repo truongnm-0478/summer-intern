cee57ac8079ff31547952b50677ecd272879c104
truongnm-0478 <ngo.mau.truong@sun-asterisk.com>
2024-07-17 18:53:22 +0700

    update

--- spring-core-modules/ex4_bean_scope_session_request_application/src/main/java/org/example/beans/ApplicationBean.java
+++ spring-core-modules/ex4_bean_scope_session_request_application/src/main/java/org/example/beans/ApplicationBean.java
@@ -1,2 +1,26 @@
-package org.example.beans;public class ApplicationBean {
+package org.example.beans;
+
+import jakarta.servlet.ServletContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletContextAware;
+
+
+@Component
+@Scope("application")
+public class ApplicationBean implements ServletContextAware {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        servletContext.setAttribute("applicationScopedBean", this);
+    }
 }
--- spring-core-modules/ex4_bean_scope_session_request_application/src/main/java/org/example/beans/SessionBean.java
+++ spring-core-modules/ex4_bean_scope_session_request_application/src/main/java/org/example/beans/SessionBean.java
@@ -1,2 +1,19 @@
-package org.example.beans;public class SessionBean {
+package org.example.beans;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
+public class SessionBean {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
